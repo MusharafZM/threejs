45feda17e57d3649feff5ab58da93c611a36f70e
Mohamed Musharaf Z <musharafzm@Mohameds-MacBook-Air.local>
2023-08-18 00:08:44 +0530

    initial commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { AnimatePresence, motion } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
