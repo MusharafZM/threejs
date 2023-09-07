0cb8cf28f5e97caaef94b9ec9cd3d9f97778da47
Mohamed Musharaf Z <musharafzm@Mohameds-MacBook-Air.local>
2023-09-07 20:48:25 +0530

    Barebones

---
+++ client/src/components/index.js
@@ -0,0 +1,11 @@
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
+import CustomButton from "./CustomButton";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
+
+export {
+    AIPicker,
+    ColorPicker, CustomButton, FilePicker,
+    Tab
+};
